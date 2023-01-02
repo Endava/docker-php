0317adfe17434c7a42d613ebbbde87f33ff2a115
DracoBlue <JanS@DracoBlue.de>
2023-01-02 23:06:20 +0100

    Move apache2/unit/phpBaseName to Dockerfile

--- build_and_push_multi_arch_images.sh
+++ build_and_push_multi_arch_images.sh
@@ -3,22 +3,17 @@
 set -e
 
 DOCKER_IMAGE_NAME=$1
-QUAY_DOCKER_IMAGE_NAME=quay.io/$DOCKER_IMAGE_NAME
-ALPINE_VERSION=$2
-PHP_VERSION=$3
-PHP_PACKAGE_BASENAME=$4
-UNIT_VERSION=$5
-APACHE2_VERSION=$6
+PHP_VERSION=$2
 TARGET_PLATFORMS=linux/arm64/v8,linux/amd64
 
-if [ ! -z "$7" ]
+if [ ! -z "$3" ]
 then
-  TARGET_PLATFORMS=$7
+  TARGET_PLATFORMS=$3
 fi
 
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 echo "Build and Push ${DOCKER_IMAGE_NAME}"
 docker buildx create --node buildx --name buildx --use
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHP_PACKAGE_BASENAME=$PHP_PACKAGE_BASENAME --build-arg UNIT_VERSION=$UNIT_VERSION --build-arg APACHE2_VERSION=$APACHE2_VERSION .
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION .
 echo "Build and Push ${QUAY_DOCKER_IMAGE_NAME}"
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHP_PACKAGE_BASENAME=$PHP_PACKAGE_BASENAME --build-arg UNIT_VERSION=$UNIT_VERSION --build-arg APACHE2_VERSION=$APACHE2_VERSION .
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION .
--- build_images.sh
+++ build_images.sh
@@ -3,16 +3,12 @@
 set -e
 
 DOCKER_IMAGE_NAME=$1
-ALPINE_VERSION=$2
-PHP_VERSION=$3
-PHP_PACKAGE_BASENAME=$4
-UNIT_VERSION=$5
-APACHE2_VERSION=$6
+PHP_VERSION=$2
 TARGET_PLATFORMS=linux/arm64/v8,linux/amd64
 
-if [ ! -z "$7" ]
+if [ ! -z "$3" ]
 then
-  TARGET_PLATFORMS=$7
+  TARGET_PLATFORMS=$3
 fi
 
 docker buildx create --node buildx --name buildx --use
@@ -21,7 +17,7 @@ docker buildx create --node buildx --name buildx --use
 for TARGET_PLATFORM in `echo $TARGET_PLATFORMS | tr -s ',' ' '`
 do
   TARGET_PLATFORM_SUFFIX=`echo $TARGET_PLATFORM | tr -s '/' '-'`
-  docker buildx build --pull --load --platform $TARGET_PLATFORM -f Dockerfile -t ${DOCKER_IMAGE_NAME}-${TARGET_PLATFORM_SUFFIX} --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHP_PACKAGE_BASENAME=$PHP_PACKAGE_BASENAME --build-arg UNIT_VERSION=$UNIT_VERSION --build-arg APACHE2_VERSION=$APACHE2_VERSION .
+  docker buildx build --pull --load --platform $TARGET_PLATFORM -f Dockerfile -t ${DOCKER_IMAGE_NAME}-${TARGET_PLATFORM_SUFFIX} --build-arg PHP_VERSION=$PHP_VERSION .
 done
 
 
