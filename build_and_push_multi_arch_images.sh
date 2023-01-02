61d4bf915c3890f323527a15b938ddc14df1cdda
DracoBlue <JanS@DracoBlue.de>
2023-01-02 23:07:29 +0100

    Also move php version to Dockerfile

--- build_and_push_multi_arch_images.sh
+++ build_and_push_multi_arch_images.sh
@@ -3,17 +3,16 @@
 set -e
 
 DOCKER_IMAGE_NAME=$1
-PHP_VERSION=$2
 TARGET_PLATFORMS=linux/arm64/v8,linux/amd64
 
-if [ ! -z "$3" ]
+if [ ! -z "$2" ]
 then
-  TARGET_PLATFORMS=$3
+  TARGET_PLATFORMS=$2
 fi
 
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 echo "Build and Push ${DOCKER_IMAGE_NAME}"
 docker buildx create --node buildx --name buildx --use
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION .
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME .
 echo "Build and Push ${QUAY_DOCKER_IMAGE_NAME}"
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION .
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME .
