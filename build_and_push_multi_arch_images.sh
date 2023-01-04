88a32c5be287813bfd934505597e25ab47a8ec70
DracoBlue <JanS@DracoBlue.de>
2023-01-04 16:58:17 +0100

    Move php version setting to Dockerfile

--- build_and_push_multi_arch_images.sh
+++ build_and_push_multi_arch_images.sh
@@ -2,19 +2,26 @@
 
 set -e
 
-DOCKER_IMAGE_NAME=$1
+PHP_VERSION=`docker run --rm -it $1 php -r 'echo PHP_VERSION;'`
+
+if [ -z "$PHP_VERSION" ]
+then
+  echo "Cannot find PHP version in docker image $1"
+  exit 1;
+fi
+
+DOCKER_IMAGE_NAME=$2:$PHP_VERSION
 QUAY_DOCKER_IMAGE_NAME=quay.io/$DOCKER_IMAGE_NAME
-PHP_VERSION=$2
 TARGET_PLATFORMS=linux/arm64/v8,linux/amd64
 
-if [ ! -z "$3" ]
+if [ ! -z "$2" ]
 then
-  TARGET_PLATFORMS=$3
+  TARGET_PLATFORMS=$2
 fi
 
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 echo "Build and Push ${DOCKER_IMAGE_NAME}"
 docker buildx create --node buildx --name buildx --use
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION .
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME
 echo "Build and Push ${QUAY_DOCKER_IMAGE_NAME}"
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME --build-arg PHP_VERSION=$PHP_VERSION .
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME
