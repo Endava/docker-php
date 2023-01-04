87518e3d86fc51c351e2937116d1ac0c26e0c737
DracoBlue <JanS@DracoBlue.de>
2023-01-04 18:05:29 +0100

    Update build_and_push_multi_arch_images.sh

--- build_and_push_multi_arch_images.sh
+++ build_and_push_multi_arch_images.sh
@@ -22,6 +22,6 @@ fi
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 echo "Build and Push ${DOCKER_IMAGE_NAME}"
 docker buildx create --node buildx --name buildx --use
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME .
 echo "Build and Push ${QUAY_DOCKER_IMAGE_NAME}"
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME
+docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME .
