2246887239e2d9793136a202d6eee37deabb3da0
DracoBlue <JanS@DracoBlue.de>
2023-01-05 19:09:36 +0100

    Try to build without buildx

--- build_images.sh
+++ build_images.sh
@@ -10,13 +10,11 @@ then
   TARGET_PLATFORMS=$2
 fi
 
-docker buildx create --node buildx --name buildx --use
-
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 for TARGET_PLATFORM in `echo $TARGET_PLATFORMS | tr -s ',' ' '`
 do
   TARGET_PLATFORM_SUFFIX=`echo $TARGET_PLATFORM | tr -s '/' '-'`
-  docker buildx build --pull --load --platform $TARGET_PLATFORM -f Dockerfile -t ${DOCKER_IMAGE_NAME}-${TARGET_PLATFORM_SUFFIX} .
+  docker build --pull --platform $TARGET_PLATFORM -f Dockerfile -t ${DOCKER_IMAGE_NAME}-${TARGET_PLATFORM_SUFFIX} .
 done
 
 
