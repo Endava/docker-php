9806454c7a658ab4f74d9be4bab88d4c2694c35e
DracoBlue <JanS@DracoBlue.de>
2023-01-05 19:11:44 +0100

    Build only linux amd64

--- build_images.sh
+++ build_images.sh
@@ -3,18 +3,20 @@
 set -e
 
 DOCKER_IMAGE_NAME=$1
-TARGET_PLATFORMS=linux/arm64/v8,linux/amd64
+TARGET_PLATFORMS=linux/amd64
 
 if [ ! -z "$2" ]
 then
   TARGET_PLATFORMS=$2
 fi
 
+docker buildx create --node buildx --name buildx --use
+
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 for TARGET_PLATFORM in `echo $TARGET_PLATFORMS | tr -s ',' ' '`
 do
   TARGET_PLATFORM_SUFFIX=`echo $TARGET_PLATFORM | tr -s '/' '-'`
-  docker build --pull --platform $TARGET_PLATFORM -f Dockerfile -t ${DOCKER_IMAGE_NAME}-${TARGET_PLATFORM_SUFFIX} .
+  docker buildx build --pull --load --platform $TARGET_PLATFORM -f Dockerfile -t ${DOCKER_IMAGE_NAME}-${TARGET_PLATFORM_SUFFIX} .
 done
 
 
