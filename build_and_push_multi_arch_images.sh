00f83e644514f29ff6295488fefc1233cc32b02c
en-jschuetze <126695184+en-jschuetze@users.noreply.github.com>
2023-06-13 13:28:48 +0200

    Show docker and docker buildx version

--- build_and_push_multi_arch_images.sh
+++ build_and_push_multi_arch_images.sh
@@ -10,6 +10,9 @@ then
   TARGET_PLATFORMS=$2
 fi
 
+docker version
+docker buildx version
+
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 echo "Build and Push ${DOCKER_IMAGE_NAME}"
 docker buildx create --node buildx --name buildx --use --driver docker-container
