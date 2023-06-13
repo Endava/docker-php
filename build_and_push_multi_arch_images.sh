21c90622c0e51e2f80d598d68d8129697a367087
en-jschuetze <126695184+en-jschuetze@users.noreply.github.com>
2023-06-13 13:22:34 +0200

    Use plain progress for docker build on 8.2-zts

--- build_and_push_multi_arch_images.sh
+++ build_and_push_multi_arch_images.sh
@@ -13,17 +13,17 @@ fi
 # we have to do it like this, because of https://github.com/docker/buildx/issues/59#issuecomment-1168619521
 echo "Build and Push ${DOCKER_IMAGE_NAME}"
 docker buildx create --node buildx --name buildx --use --driver docker-container
-docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME .
+docker buildx build --progress plain --push --platform $TARGET_PLATFORMS -f Dockerfile -t $DOCKER_IMAGE_NAME .
 if [ ! -z "$QUAY_DOCKER_IMAGE_NAME" ]
 then
   echo "Build and Push ${QUAY_DOCKER_IMAGE_NAME}"
-  docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME .
+  docker buildx build --progress plain --push --platform $TARGET_PLATFORMS -f Dockerfile -t $QUAY_DOCKER_IMAGE_NAME .
 fi
 
 if [ ! -z "$GHCR_DOCKER_IMAGE_NAME" ]
 then
   echo "Build and Push ${GHCR_DOCKER_IMAGE_NAME}"
-  docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile -t $GHCR_DOCKER_IMAGE_NAME .
+  docker buildx build --progress plain --push --platform $TARGET_PLATFORMS -f Dockerfile -t $GHCR_DOCKER_IMAGE_NAME .
 fi
 for SUFFIX in unit fpm apache2 frankenphp
 do
@@ -32,16 +32,16 @@ do
   cat files/$SUFFIX/$SUFFIX.Dockerfile.snippet.txt >> Dockerfile-${SUFFIX}
 
   echo "Build and Push ${DOCKER_IMAGE_NAME}-${SUFFIX}"
-  docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile-${SUFFIX} -t $DOCKER_IMAGE_NAME-${SUFFIX} .
+  docker buildx build --progress plain --push --platform $TARGET_PLATFORMS -f Dockerfile-${SUFFIX} -t $DOCKER_IMAGE_NAME-${SUFFIX} .
   if [ ! -z "$QUAY_DOCKER_IMAGE_NAME" ]
   then
     echo "Build and Push ${QUAY_DOCKER_IMAGE_NAME}-${SUFFIX}"
-    docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile-${SUFFIX} -t $QUAY_DOCKER_IMAGE_NAME-${SUFFIX} .
+    docker buildx build --progress plain --push --platform $TARGET_PLATFORMS -f Dockerfile-${SUFFIX} -t $QUAY_DOCKER_IMAGE_NAME-${SUFFIX} .
   fi
   if [ ! -z "$GHCR_DOCKER_IMAGE_NAME" ]
   then
     echo "Build and Push ${GHCR_DOCKER_IMAGE_NAME}-${SUFFIX}"
-    docker buildx build --push --platform $TARGET_PLATFORMS -f Dockerfile-${SUFFIX} -t $GHCR_DOCKER_IMAGE_NAME-${SUFFIX} .
+    docker buildx build --progress plain --push --platform $TARGET_PLATFORMS -f Dockerfile-${SUFFIX} -t $GHCR_DOCKER_IMAGE_NAME-${SUFFIX} .
   fi
   rm Dockerfile-${SUFFIX}
 done
